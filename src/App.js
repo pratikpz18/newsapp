a69f1f10ff7ade1257d997f84e439b43a8394105
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-04-01 15:31:49 +0530

    category props send to child

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import  NewsContainer from './components/Home';
+import  NewsContainer from './components/NewsContainer';
 import Dropdown from './components/Dropdown';
 import 'react-bootstrap';
 import React, { useState } from 'react';
 
 function App() {
-  
+  const [category,setCategory]=useState('general');
   return (
     <div className="App">
-      <Dropdown></Dropdown>
-      < NewsContainer></ NewsContainer>
+      <Dropdown setCategory={setCategory}></Dropdown>
+      <NewsContainer category={category}></ NewsContainer>
     </div>
   );
 }
